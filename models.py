a503f1c429ac7b0ff8a015b6fc1214443d102ede
Rose S Jose <rosesjose29@gmail.com>
2021-07-05 11:35:29 +0530

    Adding environment variables

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 import psycopg2
+import os
 
 def create_db():
-    dbconn = psycopg2.connect(dbname = "post")
+    dbconn = psycopg2.connect(dbname = "post", user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASS'))
     cursor = dbconn.cursor()
     f = open("schema.sql")
     sqlcode = f.read()
@@ -10,14 +11,14 @@ def create_db():
     cursor.close()
 
 def create_post(name, content):
-    dbconn = psycopg2.connect(dbname = "post")
+    dbconn = psycopg2.connect(dbname = "post", user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASS'))
     cursor = dbconn.cursor()
     cursor.execute("INSERT INTO posts(name, content) VALUES(%s, %s)", (name, content))
     dbconn.commit()
     cursor.close()
 
 def get_posts():
-    dbconn = psycopg2.connect(dbname = "post")
+    dbconn = psycopg2.connect(dbname = "post", user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASS'))
     cursor = dbconn.cursor()
     cursor.execute("SELECT * FROM posts")
     post = cursor.fetchall()
